e209c97ff2d34efa7d3d3db6b9024a9c04a4355b
ideepSight <ideepSight@users.noreply.github.com>
2025-04-27 10:14:16 +0000

    deploy: 69c3e73de2a4755893a40de58d5069cfd053a8cb

--- workflow/components/BaseNode.d.ts
+++ workflow/components/BaseNode.d.ts
@@ -1,11 +1,6 @@
 import { default as React } from 'react';
 import { DPNodeInnerData } from '../lib/baseNode';
 import { Node } from '@xyflow/react';
-export type WorkflowRetryConfig = {
-    maxRetries: number;
-    retryInterval: number;
-    retryEnabled: boolean;
-};
 export declare const nodeTypes: {
     custom: (props: Node<DPNodeInnerData>) => JSX.Element;
 };
