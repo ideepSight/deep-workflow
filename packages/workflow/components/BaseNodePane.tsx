c1c04012e44008e987d74ac0d2ac30d08869b555
kavil <kavil@qq.com>
2025-04-28 23:30:15 +0800

    完成失败重试运行

--- packages/workflow/components/BaseNodePane.tsx
+++ packages/workflow/components/BaseNodePane.tsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useMemo } from 'react';
 import './index.less';
 import { observer } from 'mobx-react-lite';
-import { Button, Divider, Empty, Input, Space, Spin, Tooltip } from '@arco-design/web-react';
+import { Button, Divider, Empty, Input, InputNumber, Popconfirm, Space, Spin, Switch, Tooltip } from '@arco-design/web-react';
 import { DPBaseNode } from '../lib';
 import { WorkfowContext } from './context';
 import { Icon } from '../../workflow/components/Icon';
-import { IconClose, IconPlayCircle, IconRecordStop } from '@arco-design/web-react/icon';
+import { IconClose, IconDelete, IconPlayCircle, IconRecordStop } from '@arco-design/web-react/icon';
 
 export const BaseNodePane: React.FC = observer(() => {
 	const { workflowIns } = useContext(WorkfowContext);
@@ -68,9 +68,9 @@ export const BaseNodePane: React.FC = observer(() => {
 					{activeNode.singleRunAble && (
 						<Tooltip content={activeNode.singleRunning ? '运行中' : '运行此步骤'}>
 							{activeNode.singleRunning ? (
-								<Button type="text" className="big-icon" icon={<Spin />} onClick={() => activeNode.runSingle()} />
+								<Button type="text" className="big-icon" icon={<Spin size={14} />} />
 							) : (
-								<Button type="text" className="big-icon" icon={<IconPlayCircle />} />
+								<Button type="text" className="big-icon" icon={<IconPlayCircle />} onClick={() => activeNode.runSingle()} />
 							)}
 						</Tooltip>
 					)}
@@ -79,6 +79,11 @@ export const BaseNodePane: React.FC = observer(() => {
 							<Button type="text" className="big-icon" icon={<IconRecordStop />} onClick={() => activeNode.stop()} />
 						</Tooltip>
 					)}
+					<Popconfirm title="确定删除此节点吗？" onOk={() => workflowIns.delNode(activeNode)}>
+						<Tooltip content="删除">
+							<Button type="text" className="big-icon" status="danger" icon={<IconDelete />} />
+						</Tooltip>
+					</Popconfirm>
 					<Divider type="vertical" />
 					<Button type="text" className="big-icon" icon={<IconClose />} onClick={() => (activeNode.active = false)} />
 				</div>
@@ -95,6 +100,34 @@ export const BaseNodePane: React.FC = observer(() => {
 				/>
 				<Divider />
 				<SetComponent node={activeNode} />
+				<Divider />
+				<b className="handle-name">失败时重试</b>
+				<Switch className="switch" size="small" checked={activeNode.data.failRetryEnable} onChange={(v) => (activeNode.data.failRetryEnable = v)} />
+				{activeNode.data.failRetryEnable && (
+					<>
+						<div className="handle-item">
+							<span className="name">最大重试次数</span>
+							<InputNumber
+								className="num-input"
+								min={1}
+								value={activeNode.data.maxRetryTimes}
+								onChange={(v) => (activeNode.data.maxRetryTimes = v)}
+								suffix={`次`}
+							/>
+						</div>
+						<div className="handle-item">
+							<span className="name">重试间隔</span>
+							<InputNumber
+								className="num-input"
+								min={0.1}
+								step={1}
+								value={activeNode.data.retryInterval}
+								onChange={(v) => (activeNode.data.retryInterval = v)}
+								suffix={`秒`}
+							/>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
